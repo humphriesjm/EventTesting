334c3687971438244d4e1ea27dc6b280af047956
Jason Humphries <humphriesj@gmail.com>
2014-01-21 17:12:05 -0500

    Initial Commit

---
+++ JHEventTesting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JHEventTesting
+//
+//  Created by Jason Humphries on 1/21/14.
+//  Copyright (c) 2014 Humphries Data Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JHEventTesting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JHEventTesting
+//
+//  Created by Jason Humphries on 1/21/14.
+//  Copyright (c) 2014 Humphries Data Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
